860fbeb196982f77a33ade70feab126b4240737c
Maxim Kruk <maximkr@playtika.com>
2020-10-27 13:26:43 +0300

    Final

---
+++ AcademyAnimations/Assets/Standard Assets/2D/Scripts/QuestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuestScript : MonoBehaviour
+{
+    private Animator _animator;
+    private bool _crouch;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        _animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        if (Input.GetKey(KeyCode.C))
+            _crouch = !_crouch;
+        _animator.SetBool("Crouch", _crouch);
+    }
+}
